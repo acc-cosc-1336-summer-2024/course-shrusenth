5ed7e575c26013951a5b6539c9e5b15f5e0b9393
shrusenth <shruthi.senthilarasu@g.austincc.edu>
2024-06-15 13:01:09 -0500

    Update main.py

--- src/homework/c_decisions/main.py
+++ src/homework/c_decisions/main.py
@@ -1,1 +1,9 @@
+import decisions
+
+options = float(input("What is the number of options "))
+total = float(input("What is the total amount ")) 
+ratio = decisions.get_options_ratio(options, total)
+rating = decisions.get_faculty_rating(ratio)
+
+print(rating)
 #
