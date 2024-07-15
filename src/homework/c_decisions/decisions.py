4d5168185dfcfc39ce66fba09043e11d108f1cb2
shrusenth <shruthi.senthilarasu@g.austincc.edu>
2024-07-15 14:55:59 -0500

    Update decisions.py

--- src/homework/c_decisions/decisions.py
+++ src/homework/c_decisions/decisions.py
@@ -1,20 +1,19 @@
-def get_options_ratio(option, total):
-  if total == 0:
-    print("Invalid: Total cannot be zero")
-return option / total
+ def get_options_ratio(option, total):
+    if total == 0:
+        print("Invalid: Total cannot be zero")
+        return 0  
+    return option / total
 
 def get_faculty_rating(ratio):
-  if (ratio >= 0.9 and ratio < 1):
+    if ratio >= 0.9 and ratio < 1:
         return "Excellent"
-    elif(ratio < 0.9 and ratio >= 0.8):
-      return "Very Good"
-    elif(ratio <.8 and ratio >= .7):
-      return "Good"
-    elif(ratio <.7 and ratio >= .6):
+    elif ratio >= 0.8 and ratio < 0.9:
+        return "Very Good"
+    elif ratio >= 0.7 and ratio < 0.8:
+        return "Good"
+    elif ratio >= 0.6 and ratio < 0.7:
         return "Needs Improvement"
-    elif(ratio <.6 and ratio >= 0):
-      return "Unnacceptable"
-else:
-return "Invalid"
-
-
+    elif ratio >= 0 and ratio < 0.6:
+        return "Unacceptable"
+    else:
+        return "Invalid"
