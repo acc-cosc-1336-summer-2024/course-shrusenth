4c76820efd05764ce2aa29fee4cad36024fcf6ff
shrusenth <shruthi.senthilarasu@g.austincc.edu>
2024-07-15 15:45:23 -0500

    Update repetition.py

--- src/homework/d_repetition/repetition.py
+++ src/homework/d_repetition/repetition.py
@@ -1,1 +1,20 @@
-#
+
+
+def get_factorial(num):
+    if num < 0:
+        return "Invalid"
+    factorial = 1
+    for i in range(1, num + 1):
+        factorial *= i
+    return factorial
+
+def sum_odd_numbers(num):
+    if num < 0:
+        return "Invalid."
+    sum_odds = 0
+    current = 1
+    while current <= num:
+        if current % 2 != 0:
+            sum_odds += current
+        current += 1
+    return sum_odds
