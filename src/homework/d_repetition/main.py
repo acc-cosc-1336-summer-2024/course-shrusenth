f01edd8f8ab277f50235da082bbfd24be2953db2
shrusenth <shruthi.senthilarasu@g.austincc.edu>
2024-07-15 15:55:07 -0500

    Update main.py

--- src/homework/d_repetition/main.py
+++ src/homework/d_repetition/main.py
@@ -1,1 +1,18 @@
-#
+import repetition
+
+def menu():
+    while True:
+        print("Homework 3 Menu")
+        print("1 - Factorial")
+        print("2 - Sum odd numbers")
+        print("3 - Exit")
+        menuoptions()
+
+def menuoptions():
+    choice = input("Choose option: ")
+    if choice == "1":
+        handle_factorials()
+    elif choice == "2":
+        handle_sum_odd_numbers()
+    else:
+      handle_exit()
