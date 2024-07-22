69848dbe135e9d82beca5f9775581d4739ef0e17
shrusenth <shruthi.senthilarasu@g.austincc.edu>
2024-07-21 23:01:59 -0500

    Hw 7

--- tests/homework/j_classes/tests_classes.py
+++ tests/homework/j_classes/tests_classes.py
@@ -1,1 +1,18 @@
-#
+import unittest
+from src.homework.j_classes.class_a import Die
+
+class TestDie(unittest.TestCase):
+    def setUp(self):
+        self.die = Die()
+    
+    def test_roll_values(self):
+        num_rolls = 3
+        valid_values = range(1, 6)
+        
+        for _ in range(num_rolls):
+            self.die.roll()
+            roll_value = self.die.get_rolled_value()
+            self.assertIn(roll_value, valid_values, "The value is out of range")
+
+if __name__ == '__main__':
+    unittest.main()
