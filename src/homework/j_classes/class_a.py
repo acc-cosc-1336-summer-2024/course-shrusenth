2d980a46d224c9ff02eaad716e22bb080c726574
shrusenth <shruthi.senthilarasu@g.austincc.edu>
2024-07-21 22:49:31 -0500

    hw 7

--- src/homework/j_classes/class_a.py
+++ src/homework/j_classes/class_a.py
@@ -1,1 +1,11 @@
-#
+import random
+
+class Die:
+    def roll(self):
+        self.__roll_value = random.randint(1, 6)
+    
+    def get_rolled_value(self):
+        return self.__roll_value
+    
+    def __str__(self):
+        return f"The rolled value is {self.__roll_value}"
