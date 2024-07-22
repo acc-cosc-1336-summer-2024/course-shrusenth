79e692c6d867b1445f89fafd7b906209bb631ffd
shrusenth <shruthi.senthilarasu@g.austincc.edu>
2024-07-21 22:58:06 -0500

    hw 7

--- src/homework/j_classes/main.py
+++ src/homework/j_classes/main.py
@@ -1,1 +1,15 @@
-#
+from class_a import die
+
+def main():
+    die_instance = die()
+    
+    while True:
+        die_instance.roll()
+        print(die_instance)
+        
+        user_input = input("Would you lile to roll again? (type (y/n)): ").strip().lower()
+        if user_input != 'y':
+            break
+
+if __name__ == "__main__":
+    main()
